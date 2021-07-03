65c612f4841cef357da63de52ce992c5d7fbf0ea
nitinrohan <69236864+nitinrohan@users.noreply.github.com>
2021-07-03 18:15:01 +0530

    Add files via upload

---
+++ Samplehello.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package samplehello;
+
+/**
+ *
+ * @author 91891
+ */
+public class Samplehello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("hello world!");
+    }
+    
+}
